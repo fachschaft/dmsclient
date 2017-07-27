30336ca755fbb87003c2014aca845f4b761ccecb
David-Elias Kuenstle <kuenstld@cs.uni-freiburg.de>
2017-07-27 11:34:06 +0200

    Increment feature version

--- dmsclient/__init__.py
+++ dmsclient/__init__.py
@@ -3,7 +3,7 @@ try:
 except ImportError as e:
     pass
 
-__version__ = '1.0.1'
+__version__ = '1.1.1'
 __author__ = 'David-Elias Künstle'
 __author_email__ = 'dmsclient@kuenstle.me'
 __description__ = 'Library and command line interface to interact with Drink Management System of Fachschaft TF Uni Freiburg.'
