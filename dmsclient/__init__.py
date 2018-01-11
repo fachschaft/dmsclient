1a46d91a45ad31d9cbbc4f6a6a7bd4cbe38b1a9b
David-Elias Kuenstle <kuenstld@cs.uni-freiburg.de>
2018-01-11 15:29:09 +0100

    Obfuscate package email to confuse spam crawlers on github

--- dmsclient/__init__.py
+++ dmsclient/__init__.py
@@ -5,5 +5,5 @@ except ImportError as e:
 
 __version__ = '1.3.0'
 __author__ = 'David-Elias Künstle'
-__author_email__ = 'dmsclient@kuenstle.me'
+__author_email__ = 'dmsclient' + chr(64) + 'kuenstle.me' # hide of dumb email crawler
 __description__ = 'Library and command line interface to interact with Drink Management System of Fachschaft TF Uni Freiburg.'
