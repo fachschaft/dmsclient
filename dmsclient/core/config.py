2e9ae41370dc658d1a721261af119ab219a2fa23
Stefan <git@moehst.de>
2018-02-28 14:53:53 +0100

    Simplify the interface of dmsConfig by encapsulating the configparser

--- dmsclient/core/config.py
+++ dmsclient/core/config.py
@@ -7,46 +7,48 @@ __all__ = ['ReadStatus', 'Sec', 'DmsConfig']
 
 
 class ReadStatus(Enum):
+    """Result enum for DmsConfig.read()"""
     NOT_FOUND = 0
     OUTDATED = 1
     LATEST = 2
 
 
 class Sec(Enum):
+    """Available config sections"""
     ALIASES = 'ALIASES'
     GENERAL = 'GENERAL'
 
 
-class DmsConfig(ConfigParser):
+class DmsConfig():
 
     def __init__(self):
         """Create a config with default values"""
-        super().__init__()
+        self._p = ConfigParser()
 
-        self.add_section(Sec.GENERAL)
-        self.set(Sec.GENERAL, 'api', 'https://dms.fachschaft.tf/api')
-        self.set(Sec.GENERAL, 'token', '')
-        self.set(Sec.GENERAL, 'version', '1')
+        self._add_section(Sec.GENERAL)
+        self._set(Sec.GENERAL, 'api', 'https://dms.fachschaft.tf/api')
+        self._set(Sec.GENERAL, 'token', '')
+        self._set(Sec.GENERAL, 'version', '1')
 
-        self.add_section(Sec.ALIASES)
-        self.set(Sec.ALIASES, 'wasser', 'Prinzen Perle')
+        self._add_section(Sec.ALIASES)
+        self._set(Sec.ALIASES, 'wasser', 'Prinzen Perle')
 
     def read(self, path):
         """Read given config. If config is an older config version it is migrated
         and can later be updated with write()
         """
-        found = super().read(path)
+        found = self._p.read(path)
         if not found:
             return ReadStatus.NOT_FOUND
 
         res = ReadStatus.LATEST
 
         # Update config to new version
-        if 'token' in self.defaults():
+        if 'token' in self._p.defaults():
             res = ReadStatus.OUTDATED
-            self.set(Sec.GENERAL, 'version', '1')
-            self.set(Sec.GENERAL, 'token', self.defaults()['token'])
-            del self.defaults()['token']
+            self._set(Sec.GENERAL, 'version', '1')
+            self._set(Sec.GENERAL, 'token', self._p.defaults()['token'])
+            del self._p.defaults()['token']
 
         # Future breaking config change can use:
         # if self._p.getint(Sec.GENERAL, 'version') == 1:
@@ -58,39 +60,35 @@ class DmsConfig(ConfigParser):
     def write(self, path):
         """Write the current state of the config into a file"""
         with open(path, 'w') as f:
-            super().write(f)
+            self._p.write(f)
 
     @property
     def api(self):
         """Api endpoint of the drink management system"""
-        return self.get(Sec.GENERAL, 'api')
+        return self._get(Sec.GENERAL, 'api')
 
     @property
     def token(self):
         """Access token for the drink management system"""
-        return self.get(Sec.GENERAL, 'token')
+        return self._get(Sec.GENERAL, 'token')
 
     @property
     def aliases(self):
         """List of aliases. Alias = (lowercase alias, mapped drink)"""
-        return [(x.lowercase(), y) for (x, y) in self.items(Sec.ALIASES)]
+        return [(x.lowercase(), y) for (x, y) in self._items(Sec.ALIASES)]
 
-    def add_section(self, sec, *args, **kwargs):
+    def _add_section(self, sec):
         """Wrapper for Configparser.add_section allowing Enum Sec as "sec"."""
-        name = sec.name if isinstance(sec, Sec) else sec
-        return super().add_section(name, *args, **kwargs)
+        return self._p.add_section(sec.name)
 
-    def items(self, sec, *args, **kwargs):
+    def _items(self, sec):
         """Wrapper for Configparser.items allowing Enum Sec as "sec"."""
-        name = sec.name if isinstance(sec, Sec) else sec
-        return super().items(name, *args, **kwargs)
+        return self._p.items(sec.name)
 
-    def get(self, sec, key, *args, **kwargs):
+    def _get(self, sec, key):
         """Wrapper for Configparser.get allowing Enum Sec as "sec"."""
-        name = sec.name if isinstance(sec, Sec) else sec
-        return super().get(name, key, *args, **kwargs)
+        return self._p.get(sec.name, key)
 
-    def set(self, sec, key, value, *args, **kwargs):
+    def _set(self, sec, key, value):
         """Wrapper for Configparser.set allowing Enum Sec as "sec"."""
-        name = sec.name if isinstance(sec, Sec) else sec
-        return super().set(name, key, value, *args, **kwargs)
+        return self._p.set(sec.name, key, value)
